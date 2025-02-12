be102121fc6680e6970deedade489103222fecda
cirroskais <cirroskais@madhouselabs.net>
2025-02-12 04:38:12 -0500

    yea

--- src/lib/Lastfm.ts
+++ src/lib/Lastfm.ts
@@ -1,64 +1,48 @@
-const KEY = process.env.LASTFM_KEY;
-const SECRET = process.env.LASTFM_SECRET;
-if (!KEY || !SECRET) throw new Error("Missing LASTFM_KEY/SECRET");
+const API_KEY = "feishinscrobbler";
 
+import { LastFMTrack, LastFMAuth } from "lastfm-ts-api";
 import { exec } from "node:child_process";
-import {
-    LastFMTrack,
-    LastFMAuth,
-    type LastFMBooleanNumber,
-    type LastFMTrackUpdateNowPlayingParams,
-    type LastFMTrackScrobbleParams,
-} from "lastfm-ts-api";
 import { read, write } from "./FlatDB";
-const lfm = new LastFMAuth(KEY, SECRET);
+const lfm = new LastFMAuth(API_KEY, "");
 
-type SessionResponse = Readonly<{
-    session: {
-        name: string;
-        key: string;
-        subscriber: LastFMBooleanNumber;
-    };
-}>;
-
-let storage = await read();
+let storage: any = await read();
 if (!storage.key) {
-    const { token } = await lfm.getToken();
-    exec(`xdg-open 'http://www.last.fm/api/auth/?api_key=${KEY}&token=${token}'`);
-    const { session }: SessionResponse = await new Promise((resolve, reject) => {
-        let i = 0;
-        const interval = setInterval(async () => {
-            if (i > 60) return reject("Took too long to authorize.");
-            i++;
-            try {
-                const session = await lfm.getSession({ token });
-                clearInterval(interval);
-                resolve(session);
-            } catch (_) {}
-        }, 1000);
+    const AUTH_URL = new URL("https://libre.fm/api/auth");
+    AUTH_URL.searchParams.set("api_key", API_KEY);
+    AUTH_URL.searchParams.set("cb", "http://localhost:1337");
+
+    exec(`xdg-open '${AUTH_URL.toString()}'`);
+    const server = Bun.serve({
+        port: 1337,
+        fetch(request) {
+            return new Promise(async (resolve) => {
+                const url = new URL(request.url);
+                const token = url.searchParams.get("token");
+                if (!token) throw new Error("NO TOKEN");
+                const { session } = await lfm.getSession({ token });
+                write(session);
+                storage = session;
+                resolve(new Response(JSON.stringify(storage)));
+                server.stop();
+                console.log("[LibreFM] Got session key");
+            }) as Promise<Response>;
+        },
     });
-    write(session);
-    storage = session;
-}
-console.log("[LastFM] Acquired session key");
+} else console.log("[LibreFM] Got session key");
 
 let lastUpdate = 0;
-export async function updateNowPlaying(params: LastFMTrackUpdateNowPlayingParams) {
-    if (!KEY || !SECRET || !storage.key) throw new Error("This error should be unreachable : 3");
-    if (Date.now() - lastUpdate < 250) return;
+export async function updateNowPlaying(params: any) {
+    if (!storage.key) return console.log("[LibreFM] Ignoring updateNowPlaying, no session");
+    if (Date.now() - lastUpdate < 1000 * 30) return;
     lastUpdate = Date.now();
-
-    console.log(`[LastFM] Now playing: ${params.artist} - ${params.track}`);
-
-    const track = new LastFMTrack(KEY, SECRET, storage.key);
+    console.log(`[LibreFM] Now playing: ${params.artist} - ${params.track}`);
+    const track = new LastFMTrack(API_KEY, "", storage.key);
     return await track.updateNowPlaying(params);
 }
 
-export async function scrobble(params: LastFMTrackScrobbleParams) {
-    if (!KEY || !SECRET || !storage.key) throw new Error("This error should be unreachable : 4");
-
-    console.log(`[LastFM] Scrobbled: ${params.artist} - ${params.track}`);
-
-    const track = new LastFMTrack(KEY, SECRET, storage.key);
+export async function scrobble(params: any) {
+    if (!storage.key) return console.log("[LibreFM] Ignoring scrobble, no session");
+    console.log(`[LibreFM] Scrobbled: ${params.artist} - ${params.track}`);
+    const track = new LastFMTrack(API_KEY, "", storage.key);
     return await track.scrobble(params);
 }
